9d5f25b7e4ae90cfc3c6dcd6cb13fe7d50aec9eb
AadityaBajgain <155723003+AadityaBajgain@users.noreply.github.com>
2025-04-02 17:19:02 -0700

    Rename User model to Pixuser

--- lib/database/models/user.model.ts
+++ lib/database/models/user.model.ts
@@ -40,6 +40,6 @@ const UserSchema = new Schema({
     }
 });
 
-const User = models?.User || model("User",UserSchema);
+const User = models?.User || model("Pixuser",UserSchema);
 
 export default User;
